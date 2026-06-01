e35e107f18deed6a201aa64e7b514f9b9436a0e8
keiharada <harada@uec.ac.jp>
2026-06-01 15:45:37 +0900

    Update gitignore

---
+++ ADT/202601/E_20260115.py
@@ -0,0 +1,141 @@
+class SegmentTree(object):
+    # 0-indexed
+    # 2^m
+    def __init__(self, length, op=min, e=lambda: 0):
+        """
+        :param length: length of initial values
+        :param op: operator, op(x, y) -> z
+        :param e: function that return identity element for op
+        """
+        self.op = op
+        self.e = e
+        self.n = 1
+        while self.n < length:
+            self.n *= 2  # pow2
+        self.dat = [e()] * (2 * self.n - 1)
+
+    def initialize(self, x_list):
+        """
+        initialize data
+        :param x_list: initial values fot list
+        :return:
+        """
+        assert len(x_list) <= self.n
+        # update base
+        # ith -> n - 1 + i
+        for i, x in enumerate(x_list):
+            self.dat[self.n - 1 + i] = x
+        # upper values
+        for i in range(self.n - 2, -1, -1):
+            self.dat[i] = self.op(self.dat[2 * i + 1], self.dat[2 * i + 2])
+
+    def set_val(self, k, a):
+        """
+        update k-th(0-indexed) value with a
+        :param k: index to set(0-indexed)
+        :param a: value to set
+        :return: None
+        """
+        k += self.n - 1
+        self.dat[k] = a
+        while k > 0:
+            k = (k - 1) // 2  # parent
+            self.dat[k] = self.op(self.dat[k * 2 + 1], self.dat[k * 2 + 2])
+
+    def query(self, p, q):
+        """
+        :param p: int
+        :param q: int
+        :return: "minimum" value in [p, q)
+        """
+        if q <= p:
+            return self.e()
+        p += self.n - 1
+        q += self.n - 2
+        vl = self.e()
+        vr = self.e()
+
+        while q - p > 1:
+            if p & 1 == 0:
+                vl = self.op(vl, self.dat[p])
+            if q & 1 == 1:
+                vr = self.op(self.dat[q], vr)
+                q -= 1
+            p = p // 2
+            q = (q - 1) // 2
+        if p == q:
+            res = self.op(self.op(vl, self.dat[p]), vr)
+        else:
+            res = self.op(self.op(vl, self.dat[p]), self.op(self.dat[q], vr))
+        return res
+
+    def query_recursive(self, p, q):
+        """
+        recursive version, Ref. Ant Book p.155
+        :param p: int
+        :param q: int
+        :return: "minimum" value in [p, q)
+        """
+        if q <= p:
+            return self.e()
+        return self._query_recursive(p, q, 0, 0, self.n)
+
+    def _query_recursive(self, a, b, k, l, r):
+        """
+        :param a: int
+        :param b: int
+        :param k: int
+        :param l: int
+        :param r: int
+        :return: "minimum" value in [a, b) and [l, r)
+        """
+        # no intersection -> e
+        if r <= a or b <= l:
+            return self.e()
+        # [a, b) includes [l, r) -> return [l, r)
+        if a <= l and r <= b:
+            return self.dat[k]
+        else:
+            # ask children
+            vl = self._query_recursive(a, b, k * 2 + 1, l, (l + r) // 2)
+            vr = self._query_recursive(a, b, k * 2 + 2, (l + r) // 2, r)
+        return self.op(vl, vr)
+
+
+def solve(n, q, s, lr_list):
+    res = []
+    x_list = [0] * n
+    for i in range(n - 1):
+        if s[i] == s[i + 1]:
+            x_list[i] = 1
+    sgt = SegmentTree(n, op=lambda x, y: x + y, e=lambda: 0)
+    sgt.initialize(x_list)
+    for l, r in lr_list:
+        res.append(sgt.query(l - 1, r - 1))
+    # print(res)
+    return res
+
+
+def main():
+    n, q = map(int, input().split())
+    s = input()
+    lr_list = [tuple(map(int, input().split())) for _ in range(q)]
+    res = solve(n, q, s, lr_list)
+    for r in res:
+        print(r)
+
+
+def test():
+    assert solve(11, 4, "mississippi", [
+        (3, 9),
+        (4, 10),
+        (4, 6),
+        (7, 7)
+    ]) == [2, 2, 0, 0]
+    assert solve(5, 1, "aaaaa", [(1, 5)]) == [4]
+
+
+if __name__ == "__main__":
+    test()
+    main()
+
